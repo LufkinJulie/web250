59fec3f8e652b419ad098cbda2ae78914f6965b8
LufkinJulie <69699210+LufkinJulie@users.noreply.github.com>
2022-05-06 08:15:58 -0400

    Add files via upload

---
+++ componets/header250_brand.php
@@ -0,0 +1,10 @@
+<header class = "background">
+	<h1 >Lufkin's Linens&nbsp;&#8466;&#8466;&nbsp</h1>
+		<nav>
+			<a href= "indexx.php?p=indexx.php" >Home</a> &nbsp;&#10247;&nbsp;
+			<a href= "indexx.php?p=brand_250.php" >Brand</a> &nbsp;&#10247;&nbsp;
+			<a href = "indexx.php?p=contract.php">Contract</a> &nbsp;&#10247;&nbsp;
+			<a href = "indexx.php?p=introduction.php">Introduction</a> &nbsp;&#10247;&nbsp;
+			<a href = "indexx.php?p=sams_used_cars\sams_used_cars.html">Sam's Used Cars</a>
+		</nav>
+</header>
